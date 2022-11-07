99e56cd7a496fa054732e6c7d8c5234104616f5f
Sizan75 <sakiuzzaman26@gmail.com>
2022-11-08 03:41:43 +0600

    jwt token install

--- index.js
+++ index.js
@@ -4,6 +4,9 @@ const jwt= require('jsonwebtoken')
 const app= express()
 const port= process.env.PORT || 5000;
 
+require('dotenv').config()
+app.use(express.json())
+app.use(cors())
 
 app.get('/',(req,res)=>{
     res.send('happily fresh server running')
