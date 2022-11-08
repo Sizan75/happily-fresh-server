1d68502b14b8e000fe7474a2d10cc2bc5f230efe
Sizan75 <sakiuzzaman26@gmail.com>
2022-11-08 11:33:06 +0600

    3 foodservice get for home done

--- index.js
+++ index.js
@@ -11,13 +11,20 @@ app.use(cors())
 
 
 
-const uri = `mongodb+srv://${process.env.DB_USER} :${process.env.DB_PASSWORD}@cluster0.bahxlpw.mongodb.net/?retryWrites=true&w=majority`;
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.bahxlpw.mongodb.net/?retryWrites=true&w=majority`;
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
 
-function run() {
+async function run() {
 try{
     const foodCollection= client.db('happilyfreshdb').collection('foodservice')
     const reviewCollection= client.db('happilyfreshdb').collection('review')
+
+    app.get('/foodservice',async(req,res)=>{
+        const query={}
+        const cursor= foodCollection.find(query)
+        const food= await cursor.limit(3).toArray();
+        res.send(food)
+    } )
 }
 finally{
 
