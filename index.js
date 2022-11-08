4e612a32467e89e92d79a9fce87a1148c5da01d0
Sizan75 <sakiuzzaman26@gmail.com>
2022-11-09 00:38:28 +0600

    private route added

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const cors=require('cors')
 const jwt= require('jsonwebtoken')
 const app= express()
 const port= process.env.PORT || 5000;
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 
 require('dotenv').config()
 app.use(express.json())
@@ -25,6 +25,18 @@ try{
         const food= await cursor.limit(3).toArray();
         res.send(food)
     } )
+    app.get('/foodservices',async(req,res)=>{
+        const query={}
+        const cursor= foodCollection.find(query)
+        const food= await cursor.toArray();
+        res.send(food)
+    } )
+    app.get('/foodservice/:id',async(req,res)=>{
+        const id= req.params.id;
+        const query={_id:ObjectId(id)}
+        const singleFood= await foodCollection.findOne(query)
+        res.send(singleFood)
+    })
 }
 finally{
 
