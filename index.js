75f6e5a2685cccfa37a41ed35d450d18e675e0c8
Sizan75 <sakiuzzaman26@gmail.com>
2022-11-10 13:26:20 +0600

    final submit

--- index.js
+++ index.js
@@ -1,6 +1,5 @@
 const express=require('express')
 const cors=require('cors')
-const jwt= require('jsonwebtoken')
 const app= express()
 const port= process.env.PORT || 5000;
 const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
@@ -13,6 +12,10 @@ app.use(cors())
 
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.bahxlpw.mongodb.net/?retryWrites=true&w=majority`;
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+
+
+
+
 
 async function run() {
 try{
@@ -54,6 +57,7 @@ try{
         res.send(foodreview);
     })
     app.get('/myreviews',async(req,res)=>{
+       
         let query = {};
 
         if (req.query.userEmail) {
